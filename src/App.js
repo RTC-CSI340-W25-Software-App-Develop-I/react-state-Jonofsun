e819ad0b34d0b4805c487939bfad6a653fc488fc
Tenshin <114845631+Jonofsun@users.noreply.github.com>
2025-03-10 16:19:46 -0700

    update comit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {data} from "./data/data.js"
 import {useState} from "react";
 import "./App.css";
 
-//App is the root of our application and where we load in our components.
 function App() {
   const [restaurantState, setRestaurants] = useState([...data])
 
